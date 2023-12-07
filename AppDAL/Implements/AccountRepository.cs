8f3cdbe4d98e91ed94f4cec2940ca954873b1bef
Dang Khoa <vodangkhoa835@gmail.com>
2023-12-08 02:14:49 +0700

    commit 2

---
+++ AppDAL/Implements/AccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppDAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDAL.Implements
+{
+    public class AccountRepository : IAccountRepository
+    {
+
+    }
+}
